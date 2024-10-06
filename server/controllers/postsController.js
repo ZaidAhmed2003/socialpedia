f8a477e2a09c771e14be48f0c6adea3c0c7bdf76
Zaid Ahmed <zaidahmed2345@gmail.com>
2024-10-06 12:15:32 +0500

    initial

---
+++ server/controllers/postsController.js
@@ -0,0 +1,29 @@
+import Post from "../models/PostModel.js"
+
+
+
+
+
+export const getFeedPosts = async(req , res ) => {
+    try {
+        
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+        
+    }
+}
+export const getUserPosts = async(req , res ) => {
+    try {
+        
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+        
+    }
+}
+export const likePost = async(req , res ) => {
+    try {
+        
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+}
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getFeedPosts,
+  getUserPosts,
+  likePost,
+} from "../controllers/postsController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// READ
+
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// Update
+
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
